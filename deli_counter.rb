a21c90b06a773ebea26634c0c18c70103d33ae64
Hannah Malm <hannahemalm616@gmail.com>
2020-11-11 01:23:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,6 +27,10 @@ end
 
 
 def now_serving(array, name)
-  
+     if array.count == 0
+      puts "There is nobody waiting to be served!"
+   else
+      puts "Currently serving #{array.shift}."
+   end
 end  
   
