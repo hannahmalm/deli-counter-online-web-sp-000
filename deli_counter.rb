66421b1dbbdeb8ae21793c4a5114120737f47c01
Hannah Malm <hannahemalm616@gmail.com>
2020-11-11 01:02:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,15 @@
 
 def katz_deli
 katz_deli = []
-  
-  
+  "The line is currently empty."
+return katz_deli  
   
 end  
+
+def assign_rooms(attendees)
+  #start a blank array to hold the message
+  message = []
+  #The room numbers are already defined in the index?
+  attendees.each_with_index{|attendees, index| message << "Hello, #{attendees}! You'll be assigned to room #{index+1}!"}
+  return message
+  end
